176be4af60f4f09d08578c97e3181fd139a84f0b
iliass elkhouyaali <iliasselkhouyaali@gmail.com>
2025-07-26 13:17:02 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/public', express.static(__dirname + '/public'));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Connection error:", err);
+});
+
+const userSchema = new mongoose.Schema({ username: String });
+const exerciseSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  description: String,
+  duration: Number,
+  date: Date,
+});
+
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+
+app.get('/', (_req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.post('/api/users', async (req, res) => {
+  const user = new User({ username: req.body.username });
+  await user.save();
+  res.json({ username: user.username, _id: user._id });
+});
+
+
+app.get('/api/users', async (_req, res) => {
+  const users = await User.find({}, 'username _id');
+  res.json(users);
+});
+
+
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  const { description, duration, date } = req.body;
+  const user = await User.findById(req.params._id);
+  if (!user) return res.status(404).json({ error: 'User not found' });
+
+  const exercise = new Exercise({
+    userId: user._id,
+    description,
+    duration: parseInt(duration),
+    date: date ? new Date(date) : new Date(),
+  });
+
+  await exercise.save();
+
+  res.json({
+    username: user.username,
+    description: exercise.description,
+    duration: exercise.duration,
+    date: exercise.date.toDateString(),
+    _id: user._id,
+  });
+});
+
+
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const { from, to, limit } = req.query;
+  const user = await User.findById(req.params._id);
+  if (!user) return res.status(404).json({ error: 'User not found' });
+
+  const query = { userId: user._id };
+  if (from || to) query.date = {};
+  if (from) query.date.$gte = new Date(from);
+  if (to) query.date.$lte = new Date(to);
+
+  let exercises = Exercise.find(query).select('description duration date');
+  if (limit) exercises = exercises.limit(parseInt(limit));
+
+  const log = (await exercises).map((e) => ({
+    description: e.description,
+    duration: e.duration,
+    date: e.date.toDateString(),
+  }));
+
+  res.json({
+    username: user.username,
+    count: log.length,
+    _id: user._id,
+    log,
+  });
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
